6314425fcd49422bc104f6ef793d66f36c1fda8d
Sabih Rahamn <sabih.562@gmail.com>
2025-06-15 17:31:30 +0500

    editing state overhaul

--- DeepSeek.py
+++ DeepSeek.py
@@ -98,17 +98,14 @@ def loadModel(knowledge_base=None):
         Ensure that the feedback includes suggestions for improvement and accuracy.
         Question: {question}
         """
-        
-        
-                #! Debugging context retrieval
 
         
         prompt = PromptTemplate( input_variables=["question"], template=prompt_template,)
         llm_chain = prompt | llm_pipeline | StrOutputParser()
         pipeline = ( {"question": RunnablePassthrough()} | llm_chain, )
         
-        pipeline = ({"context": retriever | RunnableLambda(debug_context_printer),
-                     "question": RunnablePassthrough()
-                     } | llm_chain)
+        # pipeline = ({"context": retriever | RunnableLambda(debug_context_printer),
+        #              "question": RunnablePassthrough()
+        #              } | llm_chain)
     
     return pipeline
